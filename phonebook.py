e979b2645d0b6b3b03deb56ab84ca295facfaec7
caisarus <cezar.alexandru.vasilescu@gmail.com>
2024-05-29 14:34:34 +0300

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -92,5 +92,6 @@ def add_entry():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    # from waitress import serve
+    # serve(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) 
